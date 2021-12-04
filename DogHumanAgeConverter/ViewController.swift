765b55a1818b01fb4871112b013f25d965ce1a64
Starsky <starsky.m@live.com>
2021-12-04 19:56:41 +0100

    refresh button

--- DogHumanAgeConverter/ViewController.swift
+++ DogHumanAgeConverter/ViewController.swift
@@ -13,20 +13,35 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var humanAgeLabel: UILabel!
     
+    @IBOutlet weak var convertButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        convertButton.layer.cornerRadius = 15
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 
     
     @IBAction func convertButtonDidTap(_ sender: UIButton) {
         let dogAge = Int(dogAgeTextField.text!) ?? 0
         
-        if dogAge > 20 {
-            humanAgeLabel.text = "Please give a realistic dog age."
-        } else {
+        if dogAge <= 20 {
             humanAgeLabel.text = "Your dog is \(dogAge * 7) in human years old!"
+        } else {
+            humanAgeLabel.text = "Please give a realistic dog age."
+            
         }
     }
+    
+    @IBAction func refreshButtonDidTap(_ sender: UIBarButtonItem) {
+        dogAgeTextField.text = ""
+        humanAgeLabel.text = "Please enter your dog years!"
+        view.endEditing(true)
+    }
+    
+    
 }
 
